7614d0d6e5a73b97ecf9e651f65824b289bcd71c
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-04-25 13:27:33 +0200

    Added a frontpage and a Peoplelist component

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,4 @@
+<template>
+    <li>Lag en table med personer inne i her</li>
+    <li>Og tables</li>
+</template>
---
+++ src/views/FrontView.vue
@@ -0,0 +1,6 @@
+<script setup>
+import PeopleList from '../components/PeopleList.vue';
+</script>
+<template>
+    <PeopleList></PeopleList>
+</template>
