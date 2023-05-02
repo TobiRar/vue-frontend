21e8d26c3574dcb98daf69893bf06e5b25c9ac5f
Tinaskj <106248161+Tinaskj@users.noreply.github.com>
2023-05-02 10:40:29 +0200

    Added form with v-model

--- src/components/BackHeader.vue
+++ src/components/BackHeader.vue
@@ -4,25 +4,25 @@
   </header>
 </template>
 
-<script>
-export default {
-  name: "BackHeader"
-}
+<script setup>
+// export default {
+//   name: "BackHeader"
+// }
 </script>
 
 <style scoped>
-  header {
-    background-color: rgb(38, 66, 58);
-    justify-content: left;
-  }
-  button {
-    background-color: rgb(90, 234, 146);
-    color: rgb(38, 66, 58);
-    font-weight: bolder;
-    border: 1px solid rgb(90, 234, 146);
-    border-radius: 12px;
-    padding: 4px 12px;
-    margin: 20px;
-    cursor: pointer;
-  }
+  /*header {*/
+  /*  background-color: rgb(38, 66, 58);*/
+  /*  justify-content: left;*/
+  /*}*/
+  /*button {*/
+  /*  background-color: rgb(90, 234, 146);*/
+  /*  color: rgb(38, 66, 58);*/
+  /*  font-weight: bolder;*/
+  /*  border: 1px solid rgb(90, 234, 146);*/
+  /*  border-radius: 12px;*/
+  /*  padding: 4px 12px;*/
+  /*  margin: 20px;*/
+  /*  cursor: pointer;*/
+  /*}*/
 </style>
