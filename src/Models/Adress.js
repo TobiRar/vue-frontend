bcb5bb8d35d6fd0710418370273ce34e1ddf5b7f
Kristian Granaas <Kristian.Granaas@lede.no>
2023-05-02 11:59:36 +0200

    Added models for person and adress

---
+++ src/Models/Adress.js
@@ -0,0 +1,9 @@
+export class Adress {
+    constructor(city, country, number, postNumber, streetName) {
+        this.city = city
+        this.country = country
+        this.number = number
+        this.postNumber = postNumber
+        this.streetName = streetName
+    }
+}
