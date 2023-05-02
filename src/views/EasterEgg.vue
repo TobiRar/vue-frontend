ed07e25d56d148c59eb3fe2f09ef8c65896e8fe7
Kristian Granaas <Kristian.Granaas@lede.no>
2023-05-02 15:01:05 +0200

    finished easter egg and added easteregg file and img file

---
+++ src/views/EasterEgg.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import Header from "../components/MainHeader.vue"
+    import BackHeader from "../components/BackHeader.vue"
+
+
+</script>
+
+<template>
+    <Header />
+    <BackHeader />
+   <div>
+    <img src="../Images/MicrosoftTeams-image.png">
+   </div> 
+</template>
