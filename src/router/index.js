74a35baecdd1a1bc8c72be75895897f0af528dab
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-03 13:22:51 +0200

    Added route for EditPerson

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/EasterEgg.vue"),
     },
+    {
+      path: "/edit-person",
+      name: "edit-person",
+      component: () => import("../views/EditPersonView.vue"),
+    },
   ],
 });
 
