5971443102c9daf4cc117b45de6428a79d33a0c9
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-03 10:55:39 +0200

    removed comment

--- src/RestController/Controller.js
+++ src/RestController/Controller.js
@@ -2,10 +2,6 @@ async function getPeople() {
   const response = await fetch("https://localhost:7147/all");
   const jsonData = await response.json();
   return jsonData;
-
-  /*   fetch("https://localhost:7147/all")
-    .then((res) => res.json())
-    .then((res) => console.log(res)); */
 }
 
 let list = await getPeople();
