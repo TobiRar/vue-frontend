4044040a67e859cca419aca43a43e0339f4fc747
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-03 13:22:23 +0200

    Removed Buttons

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,19 +1,13 @@
 <script setup>
 import PeopleList from '../components/PeopleList.vue';
 import Header from '../components/MainHeader.vue';
-import {getPeople} from '../RestController/Controller'
-function d(){
-    getPeople()
-}
+
 // import {computed} from "vue"
 
 
 </script>
 <template>
     <Header />
-    <button @click="d"></button>
     <PeopleList></PeopleList>
 </template>
-<style>
-
-</style>
+<style></style>
