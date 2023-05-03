c1c6c7e9b2bb2802782ba889fceacbd98bf584de
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-03 12:46:59 +0200

    proper fields for person

--- src/Models/Person.js
+++ src/Models/Person.js
@@ -1,25 +1,65 @@
-import Adress from "./Adress"
-export class Person {
-    constructor(age, birthDay, city, country, dadFirstName, dadId, dadLastName, dadName, firstName, headRadius, id, lastName, momFirstName, momId, momLastName, momName, number, postNumber, streetName) {
-        this.adress = new Adress()
-        this.adress.city = city
-        this.adress.country = country
-        this.adress.number = number
-        this.adress.postNumber = postNumber
-        this.adress.streetName = streetName
-        this.age = age
-        this.birthDay = birthDay
-        this.dadFirstName = dadFirstName
-        this.dadId = dadId
-        this.dadLastName = dadLastName
-        this.dadName = dadFirstName + " " + dadLastName
-        this.firstName = firstName
-        this.headRadius = Number(headRadius)
-        this.id = id
-        this.lastName = lastName
-        this.momFirstName = momFirstName
-        this.momId = momId
-        this.momLastName = momLastName
-        this.momName = momFirstName + " " + momLastName
-    }
+export default class Person {
+  id;
+  firstName;
+  lastName;
+  age;
+  headRadius;
+  birthDay;
+  momId;
+  dadId;
+  dadFirstName;
+  dadLastName;
+  momFirstName;
+  momLastName;
+  streetName;
+  number;
+  city;
+  postNumber;
+  country;
+  constructor(person) {}
 }
+
+/* export default class Person {
+  constructor(
+    age,
+    birthDay,
+    city,
+    country,
+    dadFirstName,
+    dadId,
+    dadLastName,
+    dadName,
+    firstName,
+    headRadius,
+    id,
+    lastName,
+    momFirstName,
+    momId,
+    momLastName,
+    momName,
+    number,
+    postNumber,
+    streetName
+  ) {
+    this.adress = new Adress();
+    this.adress.city = city;
+    this.adress.country = country;
+    this.adress.number = number;
+    this.adress.postNumber = postNumber;
+    this.adress.streetName = streetName;
+    this.age = age;
+    this.birthDay = birthDay;
+    this.dadFirstName = dadFirstName;
+    this.dadId = dadId;
+    this.dadLastName = dadLastName;
+    this.dadName = dadFirstName + " " + dadLastName;
+    this.firstName = firstName;
+    this.headRadius = Number(headRadius);
+    this.id = id;
+    this.lastName = lastName;
+    this.momFirstName = momFirstName;
+    this.momId = momId;
+    this.momLastName = momLastName;
+    this.momName = momFirstName + " " + momLastName;
+  }
+} */
