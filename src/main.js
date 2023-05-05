0f02f1622dde658b77fe96b163f9e2dfe95f0c50
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-05 13:42:45 +0200

    fuck font awesome all my homies hate font awesome

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-library.add(faUserSecret)
 
-const app = createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
+const app = createApp(App);
 app.use(router);
 app.mount("#app");
