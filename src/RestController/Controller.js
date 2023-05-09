3aa22105cafbc5ad005ab10cd6d3ad57fa1cb4cc
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-09 12:30:16 +0200

    Creating and editing a person now returns to front page

--- src/RestController/Controller.js
+++ src/RestController/Controller.js
@@ -31,7 +31,7 @@ async function searchPerson(name) {
   console.log(jsonData);
 }
 
-async function createPerson() {
+async function createPerson(callback) {
   const Person = usePerson();
   const response = await fetch("https://localhost:7147/CreatePerson", {
     method: "POST",
@@ -41,6 +41,7 @@ async function createPerson() {
     body: JSON.stringify(Person.Person),
   });
   console.log(response);
+  callback();
 }
 
 async function editPerson() {
