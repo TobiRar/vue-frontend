1ca6176471640223cd535edb5f34a3a2e31c93f0
Tobias Reidarsen <127215720+TobiRar@users.noreply.github.com>
2023-05-09 15:43:05 +0200

    now asks the user before reloading if they have made any inputs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+const beforeUnloadListener = (event) => {
+  event.preventDefault();
+  return (event.returnValue = "");
+};
+
+const nameInput = document.querySelector("body");
+
+nameInput.addEventListener("input", (event) => {
+  if (event.target.value !== "") {
+    addEventListener("beforeunload", beforeUnloadListener, { capture: true });
+  } else {
+    removeEventListener("beforeunload", beforeUnloadListener, {
+      capture: true,
+    });
+  }
+});
 
 const app = createApp(App);
 app.use(router);
